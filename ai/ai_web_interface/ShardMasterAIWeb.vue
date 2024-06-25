1200273e02a875c01096ba42deb3549ebae35303
KOSASIH <kosasihg88@gmail.com>
2024-06-25 08:06:27 +0700

    Create ShardMasterAIWeb.vue

---
+++ ai/ai_web_interface/ShardMasterAIWeb.vue
@@ -0,0 +1,34 @@
+// ShardMasterAIWeb.vue
+<template>
+  <div>
+    <h1>ShardMaster AI Web Interface</h1>
+    <input type="text" v-model="inputData" />
+    <button @click="makePrediction">Make Prediction</button>
+    <div v-if="output">
+      <h2>Prediction: {{ output }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputData: '',
+      output: null
+    }
+  },
+  methods: {
+    makePrediction() {
+      // Make HTTP request to ShardMaster AI API
+      axios.post('/predict', { input_data: this.inputData })
+       .then(response => {
+          this.output = response.data
+        })
+       .catch(error => {
+          console.error(error)
+        })
+    }
+  }
+}
+</script>
